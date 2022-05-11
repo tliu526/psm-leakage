11076ea521eb1f6c55367b83c9deb3798e815976
Tony Liu <tliu0526@gmail.com>
2022-05-11 11:55:13 -0400

    add crosshairs, weighted regression plots

--- utils.py
+++ utils.py
@@ -10,7 +10,8 @@ def compute_study_pairs(ps_rct_df):
         rct_df = group[group['study_type'] == 'RCT']
         for ps_idx, ps in ps_df.iterrows():
             ps_dict = {
-                "study_id": [],
+                "ps_study_id": [],
+                "rct_study_id": [],
                 "rct_year": [],
                 "ps_year": [],
                 "year_diff": [],
@@ -19,10 +20,13 @@ def compute_study_pairs(ps_rct_df):
                 "clinical_setting": [],
                 'rct_ci_diff': [],
                 'ps_ci_diff': [],
+                'rct_upper_ci': [],
+                'ps_upper_ci': [],
                 "meta_study": []
             }
             for rct_idx, rct in rct_df.iterrows():
-                ps_dict['study_id'].append(ps['study_id'])
+                ps_dict['ps_study_id'].append(ps['study_id'])
+                ps_dict['rct_study_id'].append(rct['study_id'])
                 ps_dict['ps_year'].append(ps['year'])
                 ps_dict['rct_year'].append(rct['year'])
                 ps_dict['year_diff'].append(ps['year'] - rct['year'])
@@ -31,6 +35,8 @@ def compute_study_pairs(ps_rct_df):
                 ps_dict['clinical_setting'].append(ps['clinical_setting'])
                 ps_dict['rct_ci_diff'].append(rct['ci_diff'])
                 ps_dict['ps_ci_diff'].append(ps['ci_diff'])
+                ps_dict['rct_upper_ci'].append(rct['upper_ci'])
+                ps_dict['ps_upper_ci'].append(ps['upper_ci'])
                 ps_dict['meta_study'].append(ps['meta_study'])
 
             
